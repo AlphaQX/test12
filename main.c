b4df385a9c3ddc78bb62d4b81457a9ef980ca5bc
Shivam Mishra <shivam@bat.alpha.wayne>
2026-05-27 15:43:59 +0530

    timepass

--- main.c
+++ main.c
@@ -3,7 +3,7 @@
 int main() {
     int a, b;
 
-    printf("Enter the first number: ");
+    printf("Enter the first numbers: ");
     scanf("%d", &a);
 
     printf("Enter the second number now: ");
